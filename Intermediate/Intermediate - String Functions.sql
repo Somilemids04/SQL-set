9f1420645864be263c761ba9fec0a87c41b0423e
Somilemids04 <gsomil573@gmail.com>
2026-07-04 15:16:10 +0530

    updated

--- Intermediate/Intermediate - String Functions.sql
+++ Intermediate/Intermediate - String Functions.sql
@@ -1,86 +1,210 @@
-#Now let's look at string functions. These help us change and look at strings differently.
+/*
+===========================================
+SQL String Functions
+===========================================
 
-SELECT * 
-FROM bakery.customers;
+Description:
+String functions are used to manipulate, transform,
+and analyze text values in SQL.
+
+Common Uses:
+1. Converting text case
+2. Removing whitespace
+3. Extracting characters
+4. Replacing text
+5. Searching within strings
+6. Concatenating strings
+
+===========================================
+*/
 
 
-#Length will give us the length of each value
-SELECT LENGTH('sky');
+/*--------------------------------------------------
+LENGTH()
+----------------------------------------------------
 
-#Now we can see the length of each name
-SELECT first_name, LENGTH(first_name) 
+Returns the number of characters in a string.
+
+--------------------------------------------------*/
+
+SELECT LENGTH('SQL');
+
+SELECT
+    first_name,
+    LENGTH(first_name) AS name_length
 FROM employee_demographics;
 
-#Upper will change all the string characters to upper case
-SELECT UPPER('sky');
 
-SELECT first_name, UPPER(first_name) 
+/*--------------------------------------------------
+UPPER()
+----------------------------------------------------
+
+Converts all characters to uppercase.
+
+--------------------------------------------------*/
+
+SELECT UPPER('sql');
+
+SELECT
+    first_name,
+    UPPER(first_name) AS uppercase_name
 FROM employee_demographics;
 
-#lower will change all the string characters to lower case
-SELECT LOWER('sky');
 
-SELECT first_name, LOWER(first_name) 
+/*--------------------------------------------------
+LOWER()
+----------------------------------------------------
+
+Converts all characters to lowercase.
+
+--------------------------------------------------*/
+
+SELECT LOWER('SQL');
+
+SELECT
+    first_name,
+    LOWER(first_name) AS lowercase_name
 FROM employee_demographics;
 
-#Now if you have values that have white space on the front or end, we can get rid of that white space using TRIM
-SELECT TRIM('sky'   );
 
-#Now if we have white space in the middle it doesn't work
-SELECT LTRIM('     I           love          SQL');
+/*--------------------------------------------------
+TRIM()
+----------------------------------------------------
 
-#There's also L trim for trimming just the left side
+Removes leading and trailing whitespace.
+
+--------------------------------------------------*/
+
+SELECT TRIM('   SQL Tutorial   ');
+
+
+/*--------------------------------------------------
+LTRIM()
+----------------------------------------------------
+
+Removes whitespace from the left side only.
+
+--------------------------------------------------*/
+
 SELECT LTRIM('     I love SQL');
 
 
-#There's also R trim for trimming just the Right side
-SELECT RTRIM('I love SQL    ');
+/*--------------------------------------------------
+RTRIM()
+----------------------------------------------------
+
+Removes whitespace from the right side only.
+
+--------------------------------------------------*/
+
+SELECT RTRIM('I love SQL     ');
 
 
-#Now we have Left. Left is going to allow us to take a certain amount of strings from the left hand side.
+/*--------------------------------------------------
+LEFT()
+----------------------------------------------------
+
+Returns the specified number of characters
+from the beginning of a string.
+
+--------------------------------------------------*/
+
 SELECT LEFT('Alexander', 4);
 
-SELECT first_name, LEFT(first_name,4) 
+SELECT
+    first_name,
+    LEFT(first_name, 4) AS first_four_characters
 FROM employee_demographics;
 
-#Right is basically the opposite - taking it starting from the right side
+
+/*--------------------------------------------------
+RIGHT()
+----------------------------------------------------
+
+Returns the specified number of characters
+from the end of a string.
+
+--------------------------------------------------*/
+
 SELECT RIGHT('Alexander', 6);
 
-SELECT first_name, RIGHT(first_name,4) 
+SELECT
+    first_name,
+    RIGHT(first_name, 4) AS last_four_characters
 FROM employee_demographics;
 
-#Now let's look at substring, this one I personally love and use a lot.
-#Substring allows you to specify a starting point and how many characters you want so you can take characters from anywhere in the string. 
+
+/*--------------------------------------------------
+SUBSTRING()
+----------------------------------------------------
+
+Extracts a portion of a string.
+
+Syntax:
+SUBSTRING(string, start_position, length)
+
+--------------------------------------------------*/
+
 SELECT SUBSTRING('Alexander', 2, 3);
 
-#We could use this on phones to get the area code at the beginning.
-SELECT birth_date, SUBSTRING(birth_date,1,4) as birth_year
+SELECT
+    birth_date,
+    SUBSTRING(birth_date, 1, 4) AS birth_year
 FROM employee_demographics;
 
-#We can also use replace
-SELECT REPLACE(first_name,'a','z')
+
+/*--------------------------------------------------
+REPLACE()
+----------------------------------------------------
+
+Replaces occurrences of a substring
+with another substring.
+
+--------------------------------------------------*/
+
+SELECT
+    first_name,
+    REPLACE(first_name, 'a', 'z') AS modified_name
 FROM employee_demographics;
 
-#Next we have locate - we have 2 arguments we can use here: we can specify what we are searching for and where to search
-#It will return the position of that character in the string.
+
+/*--------------------------------------------------
+LOCATE()
+----------------------------------------------------
+
+Returns the position of the first occurrence
+of a substring within a string.
+
+Returns:
+0 -> Not found
+
+--------------------------------------------------*/
+
 SELECT LOCATE('x', 'Alexander');
 
-#Now Alexander has 2 e's - what will happen if we try to locate it
 SELECT LOCATE('e', 'Alexander');
-#It will return the location of just the first position.
 
-#Let's try it on our first name
-SELECT first_name, LOCATE('a',first_name) 
+SELECT
+    first_name,
+    LOCATE('a', first_name) AS position_of_a
 FROM employee_demographics;
 
-#You can also locate longer strings
-SELECT first_name, LOCATE('Mic',first_name) 
+SELECT
+    first_name,
+    LOCATE('Mic', first_name) AS search_result
 FROM employee_demographics;
 
-#Now let's look at concatenate - it will combine the strings together
-SELECT CONCAT('Alex', 'Freberg');
 
-#Here we can combine the first and the last name columns together
-SELECT CONCAT(first_name, ' ', last_name) AS full_name
+/*--------------------------------------------------
+CONCAT()
+----------------------------------------------------
+
+Combines two or more strings into one.
+
+--------------------------------------------------*/
+
+SELECT CONCAT('Alex', ' ', 'Freberg');
+
+SELECT
+    CONCAT(first_name, ' ', last_name) AS full_name
 FROM employee_demographics;
-
